0e63c88ed0cb9f0fd909a0a79a401656ac67be1d
Imron Rosdiana <imron02@gmail.com>
2015-05-17 17:19:39 +0700

    Update gitignore

---
+++ login/form_login_model.cpp
@@ -0,0 +1,5 @@
+#include "form_login_model.h"
+
+FormLoginModel::FormLoginModel()
+{
+}
---
+++ login/form_login_model.h
@@ -0,0 +1,10 @@
+#ifndef FORM_LOGIN_MODEL_H
+#define FORM_LOGIN_MODEL_H
+
+class FormLoginModel
+{
+public:
+    FormLoginModel();
+};
+
+#endif // FORM_LOGIN_MODEL_H
